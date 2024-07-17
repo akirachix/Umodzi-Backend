bb0d18eea2b698de1767186d84bbf4ed91b2a2be
student <faithnyamburawamui@gmail.com>
2024-07-17 06:54:42 +0300

    made changes

--- providers/models.py
+++ providers/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from users.models import Users
 
 # Create your models here.
 
 class Providers(models.Model):
-    user_id=models.SmallIntegerField()
+    user_id=models.ForeignKey(Users, on_delete=models.CASCADE)
     provider_name=models.CharField(max_length=30)
     billing_details=models.CharField(max_length =60)
 
--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    user_id=models.SmallIntegerField()
+    user_id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=30)
     contact_details=models.CharField(max_length=30)
     balance=models.SmallIntegerField()
