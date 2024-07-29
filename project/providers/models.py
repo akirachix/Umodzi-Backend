05febd044207f5a75b9c862ad45ba8d31d2f7958
student <faithnyamburawamui@gmail.com>
2024-07-29 12:25:07 +0300

    made changes

---
+++ project/providers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import Users
+
+# Create your models here.
+
+class Providers(models.Model):
+    user_id=models.ForeignKey(Users, on_delete=models.CASCADE)
+    provider_name=models.CharField(max_length=30)
+    billing_details=models.CharField(max_length =60)
+
+def _str_(self):
+    return f'{self.provider_name} {self.billing_details}'
